c0fcefd34d467b38bd37bc98c976be31e5bb4b84
WowDogeCode <anil.basol34@gmail.com>
2025-02-20 22:48:17 +0300

    Test added to console for business function

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+        foreach (var product in productManager.GetAllProducts())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
