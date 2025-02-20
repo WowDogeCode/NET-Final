ff589016b3bf9e507f565579afff7c5462bc2a5a
WowDogeCode <anil.basol34@gmail.com>
2025-02-20 22:46:31 +0300

    Business class and interface added.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAllProducts()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
