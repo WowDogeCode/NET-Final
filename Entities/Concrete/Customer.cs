dedb0915ca6ee0576e61330ee2821eef97957efb
WowDogeCode <anil.basol34@gmail.com>
2025-02-23 22:06:51 +0300

    Customer entity added.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        // CustomerId type set to string, because NorthwindDB holds CustomerId as string
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
