1fc51e0d4a00fdff081f8e44abfd2d942b8fec68
WowDogeCode <anil.basol34@gmail.com>
2025-02-23 22:13:36 +0300

    Console test for EntityFramework.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,21 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 class Program()
 {
     static void Main(string[] args)
     {
+        //InMemory test
         ProductManager productManager = new ProductManager(new InMemoryProductDal());
         foreach (var product in productManager.GetAllProducts())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+
+        //EfTest
+        ProductManager productManager2 = new ProductManager(new EfProductDal());
+        foreach(var product in productManager2.GetAllProducts())
         {
             Console.WriteLine(product.ProductName);
         }
