0b656fbb5b7c35c637a246b546bbdce004450573
WowDogeCode <anil.basol34@gmail.com>
2025-02-26 17:58:22 +0300

    Console tests arranged and Order test added.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,30 +6,60 @@ class Program()
 {
     static void Main(string[] args)
     {
-        //InMemory test
+        InMemoryProductTest();
+        EfProductGetAllProductsTest();
+        EfProductGetAllByCategoryId(2);
+        EfProductGetByUnitPriceRange(40, 100);
+        EfOrderGetAllOrdersTest();
+    }
+
+    private static void InMemoryProductTest()
+    {
         ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
         foreach (var product in productManager.GetAllProducts())
         {
             Console.WriteLine(product.ProductName);
         }
+    }
 
-        //EfTest
-        ProductManager productManager2 = new ProductManager(new EfProductDal());
-        foreach (var product in productManager2.GetAllProducts())
+    private static void EfProductGetAllProductsTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetAllProducts())
         {
             Console.WriteLine(product.ProductName);
         }
+    }
 
-        //Ef GetByCategoryId test
-        foreach (var product in productManager2.GetByCategoryId(2))
+    private static void EfProductGetAllByCategoryId(int categoryId)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetByCategoryId(categoryId))
         {
             Console.WriteLine(product.ProductName);
         }
+    }
 
-        //Ef GetByUnitPriceRange test
-        foreach (var product in productManager2.GetByUnitPriceRange(40, 100))
+    private static void EfProductGetByUnitPriceRange(int min, int max)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetByUnitPriceRange(min, max))
         {
             Console.WriteLine(product.ProductName);
+        }
+    }
+
+    private static void EfOrderGetAllOrdersTest()
+    {
+        OrderManager orderManager = new OrderManager(new EfOrderDal());
+
+        foreach (var order in orderManager.GetAllOrders())
+        {
+            Console.WriteLine(order.ShipCity);
         }
     }
 }
