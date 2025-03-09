cbca2735c07e48bcb0153e0e4cb0cd6c69605834
WowDogeCode <User@DESKTOP-4V9C9KE>
2025-03-09 21:59:32 +0300

    Messages class added.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product has been added";
+        public static string ProductNameInvalid = "Product name is invalid";
+    }
+}
