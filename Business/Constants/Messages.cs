c0ca10454f360c1dbfd81adbf167faf6b9353707
WowDogeCode <anil.basol34@gmail.com>
2025-03-10 17:30:15 +0300

    New fields added to Messages.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -4,5 +4,6 @@
     {
         public static string ProductAdded = "Product has been added";
         public static string ProductNameInvalid = "Product name is invalid";
+        public static string MaintenanceTime = "System is not accessible due to maintenance";
     }
 }
