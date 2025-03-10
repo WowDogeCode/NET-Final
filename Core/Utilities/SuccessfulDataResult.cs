c125a43fa5ee92f53e737e2597a20729b7c0fb28
WowDogeCode <anil.basol34@gmail.com>
2025-03-10 17:30:42 +0300

    ErrorDateResult and SuccessfulDataResult classes are implemented.

---
+++ Core/Utilities/SuccessfulDataResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Utilities
+{
+    public class SuccessfulDataResult<T> : DataResult<T>
+    {
+        public SuccessfulDataResult(T data) : base(data, true) { }
+        public SuccessfulDataResult(T data, string message) : base(data, true, message) { }
+        public SuccessfulDataResult(string message) : base(default, true, message) { }
+        public SuccessfulDataResult() : base(default, true) { }
+    }
+}
