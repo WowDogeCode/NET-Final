a33850c6e6ec60b777b80fbb5a8b616463c8e30f
WowDogeCode <anil.basol34@gmail.com>
2025-03-24 23:03:44 +0300

    Methods added for ICategoryService and implemented.

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        IDataResult<List<Category>> GetAllCategories();
+        IDataResult<Category> GetById(int categoryId);
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IDataResult<List<Category>> GetAllCategories()
+        {
+            return new SuccessfulDataResult<List<Category>>(_categoryDal.GetAll());
+        }
+
+        public IDataResult<Category> GetById(int categoryId)
+        {
+            return new SuccessfulDataResult<Category>(_categoryDal.Get(x => x.CategoryId == categoryId));
+        }
+    }
+}
