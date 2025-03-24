6b5997e631f96665a8ad6d6e38f27084164fa33d
WowDogeCode <anil.basol34@gmail.com>
2025-03-24 23:05:03 +0300

    BusinessRules implemented for business logic.

---
+++ Core/Business/BusinessRules.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+
+namespace Core.Business
+{
+    public static class BusinessRules
+    {
+        public static IResult? Check(params IResult[] rules)
+        {
+            foreach (var rule in rules)
+            {
+                if (!rule.IsSuccess)
+                {
+                    return rule;
+                }
+            }
+
+            return null;
+        }
+    }
+}
