9d369803a3c20efa9462635b3d19b6d255ff3e06
WowDogeCode <anil.basol34@gmail.com>
2025-03-24 23:05:48 +0300

    Business logic added to ProductManager.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,7 +2,7 @@
 using Business.Constants;
 using Business.ValidationRules;
 using Core.Aspects.Validation;
-using Core.CrossCuttingConcerns.Validation;
+using Core.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -13,9 +13,11 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-        public ProductManager(IProductDal productDal)
+        ICategoryService _categoryService;
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         public IDataResult<List<Product>> GetAllProducts()
@@ -51,13 +53,20 @@ namespace Business.Concrete
         [ValidationAspect(typeof(ProductValidator))]
         public IResult AddProduct(Product product)
         {
-            if (IsProductCanBeAddedForCategory(product.CategoryId))
+            IResult? ruleError = BusinessRules.Check(
+                CheckIfProductCanBeAddedForCategory(product.CategoryId),
+                CheckIfProductCanBeAddedWithName(product.ProductName),
+                CheckIfCategoryCountExceeds()
+                );
+
+            if (ruleError == null)
             {
                 _productDal.Add(product);
+
                 return new SuccessfulResult(Messages.ProductAdded);
             }
 
-            return new ErrorResult(Messages.ProductCountExceedForCategory);
+            return ruleError;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
@@ -65,19 +74,50 @@ namespace Business.Concrete
         {
             Product productToUpdate = _productDal.Get(x => x.ProductId == product.ProductId);
 
-            if(productToUpdate == null)
+            if (productToUpdate == null)
             {
-                return new ErrorResult("Product not found");
+                return new ErrorResult(Messages.ProductNotFound);
             }
-            
+
             _productDal.Update(product);
 
-            return new SuccessfulResult("Product has been updated");
+            return new SuccessfulResult(Messages.ProductUpdated);
         }
 
-        private bool IsProductCanBeAddedForCategory(int categoryId)
+        private IResult CheckIfProductCanBeAddedForCategory(int categoryId)
         {
-            return _productDal.GetAll(x => x.CategoryId == categoryId).Count <= 10;
+            var productsCount = _productDal.GetAll(x => x.CategoryId == categoryId).Count;
+
+            if (productsCount <= 10)
+            {
+                return new SuccessfulResult();
+            }
+
+            return new ErrorResult(Messages.ProductCountExceedForCategory);
+        }
+
+        private IResult CheckIfProductCanBeAddedWithName(string productName)
+        {
+            bool isProductNameExists = _productDal.Get(x => x.ProductName == productName) != null ? true : false;
+
+            if (isProductNameExists)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+
+            return new SuccessfulResult();
+        }
+
+        private IResult CheckIfCategoryCountExceeds()
+        {
+            var categoriesCount = _categoryService.GetAllCategories().Data.Count;
+
+            if (categoriesCount >= 15)
+            {
+                return new ErrorResult(Messages.CategoryCountExceeds);
+            }
+
+            return new SuccessfulResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -5,6 +5,10 @@
         public static string ProductAdded = "Product has been added";
         public static string ProductNameInvalid = "Product name is invalid";
         public static string MaintenanceTime = "System is not accessible due to maintenance";
-        public static string ProductCountExceedForCategory = "Product amount for this category more than 10";
+        public static string ProductCountExceedForCategory = "Product amount for this category is more than 10";
+        public static string ProductNameAlreadyExists = "Product name already exists";
+        public static string CategoryCountExceeds = "Category count exceeds 15";
+        public static string ProductUpdated = "Product has been updated";
+        public static string ProductNotFound = "Product is not found";
     }
 }
