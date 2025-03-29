8e8df070a5ff0677d076092c62836a50272f478c
WowDogeCode <anil.basol34@gmail.com>
2025-03-29 23:15:15 +0300

    IUserService and UserManager are implemented.

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<OperationClaim>> GetOperationClaims(User user);
+        IResult AddUser(User user);
+        IDataResult<User> GetByEmail(string email);
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserDal _userDal;
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public IResult AddUser(User user)
+        {
+            _userDal.Add(user);
+
+            return new SuccessfulResult(Messages.UserAdded);
+        }
+
+        public IDataResult<User> GetByEmail(string email)
+        {
+            return new SuccessfulDataResult<User>(_userDal.Get(x => x.Email == email));
+        }
+
+        public IDataResult<List<OperationClaim>> GetOperationClaims(User user)
+        {
+            return new SuccessfulDataResult<List<OperationClaim>>(_userDal.GetOperationClaims(user));
+        }
+    }
+}
