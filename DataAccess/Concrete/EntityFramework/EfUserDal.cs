e56e50b16b4d1d14c907e94a97e8e6c090c041f7
WowDogeCode <anil.basol34@gmail.com>
2025-03-29 23:14:03 +0300

    IUserDal and UserDal are implemented.

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,27 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetOperationClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from uoc in context.UserOperationClaims
+                             join oc in context.OperationClaims
+                             on uoc.OperationClaimId equals oc.Id
+                             where uoc.UserId == user.Id
+                             select new OperationClaim
+                             {
+                                 Id = oc.Id,
+                                 Name = oc.Name
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+}
