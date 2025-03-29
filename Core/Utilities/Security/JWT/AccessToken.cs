5587b4f545bd1555eb07a4a5b4cbabaf8ce82668
WowDogeCode <anil.basol34@gmail.com>
2025-03-29 23:18:36 +0300

    JWT generation implementation.

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken
+    {
+        public required string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ Core/Utilities/Security/JWT/TokenOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Utilities.Security.JWT
+{
+    public class TokenOptions
+    {
+        public required string Audience { get; set; }
+        public required string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public required string SecurityKey { get; set; }
+    }
+}
