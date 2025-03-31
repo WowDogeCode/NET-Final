2a49cdf7044dc018d975e7140ee28128539e931f
WowDogeCode <anil.basol34@gmail.com>
2025-03-31 20:30:37 +0300

    HashingHelper implemented with Pbkdf2 algorithm.

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public static class HashingHelper
+    {
+        private const int SaltSize = 64;
+        private const int HashSize = 128;
+        private const int Iterations = 10000;
+
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            passwordSalt = RandomNumberGenerator.GetBytes(SaltSize);
+            passwordHash = Rfc2898DeriveBytes.Pbkdf2(password, passwordSalt, Iterations, HashAlgorithmName.SHA512, HashSize);
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, passwordSalt, Iterations, HashAlgorithmName.SHA512, HashSize);
+
+            return CryptographicOperations.FixedTimeEquals(inputHash, passwordHash);
+        }
+    }
+}
